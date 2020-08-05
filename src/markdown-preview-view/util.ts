793a8bd50397225971c6f9c454b7458bc086fa1d
Harsha <harshakokel@gmail.com>
2020-08-05 13:31:18 -0500

    custom changes for my blog posts.

--- src/markdown-preview-view/util.ts
+++ src/markdown-preview-view/util.ts
@@ -208,12 +208,103 @@ export function mkHtml(
 <html data-markdown-preview-plus-context="html-export">
   <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="../assets/css/main.css" />
     <title>${title}</title>${maybeMathJaxScript}
     <style>${getMarkdownPreviewCSS()}</style>
 ${html.head!.innerHTML}
   </head>
   <body>
+  <!-- Wrapper -->
+<div id="wrapper">
+
+<!-- Main -->
+<div id="main">
+  <div class="inner">
+
+<header id="header">
+<h3><a href="../index.html">Harsha Kokel</a></h3>
+<ul class="icons">
+<li id="linkedin">
+  <a target="_blank" href="http://linkedin.com/in/harshakokel" title="linkedin" class="icon fa-linkedin">
+    <span class="label" >Linked in</span>
+  </a>
+</li>
+<li id="github">
+  <a target="_blank" href="https://github.com/harshakokel" title="github" class="icon fa-github">
+    <span class="label" >Github</span>
+  </a>
+</li>
+<li id="email">
+  <a  href="mailto:hkokel@utdallas.edu" title="mail" class="icon fa-envelope" >
+    <span class="label" >Mail</span>
+  </a>
+</li>
+<li id="twitter">
+  <a target="_blank" href="https://twitter.com/harsha_kokel" title="twitter" class="icon fa-twitter" >
+    <span class="label" title="Twitter">Twitter</span>
+  </a>
+</li>
+</ul>
+</header>
+
+<div class="page__section">
+<nav class="breadcrumb breadcrumb_type5" aria-label="Breadcrumb">
+  <ol class="breadcrumb__list r-list">
+    <li class="breadcrumb__group">
+      <a href="../" class="breadcrumb__point r-link">Home</a>
+      <span class="breadcrumb__divider" aria-hidden="true">&raquo;</span>
+    </li>
+    <li class="breadcrumb__group">
+      <a href="./" class="breadcrumb__point r-link">Blog</a>
+      <span class="breadcrumb__divider" aria-hidden="true">&raquo;</span>
+    </li>
+    <li class="breadcrumb__group">
+      <span class="breadcrumb__point" aria-current="page">Post</span>
+    </li>
+  </ol>
+</nav>
+</div>
+
     ${html.body.innerHTML}
+
+    </div>
+    </div>
+
+    <!-- Sidebar -->
+    <div id="sidebar" class="inactive">
+      <div class="inner">
+
+        <!-- Menu -->
+        <nav id="menu">
+          <header class="major">
+            <h2>Menu</h2>
+          </header>
+          <ul>
+            <li><a href="../">Homepage</a></li>
+            <li><a href="../cv.html">CV</a></li>
+            <li><a href="./">Blog Posts</a></li>
+          </ul>
+        </nav>
+
+        <!-- Footer -->
+        <footer id="footer">
+          <p class="copyright"> Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+        </footer>
+
+      </div>
+    </div>
+
+    </div>
+
+	<!-- Scripts -->
+
+	<script src="../assets/js/jquery.min.js"></script>
+	<script src="../assets/js/skel.min.js"></script>
+	<script src="../assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="../assets/js/ie/respond.min.js"></script><![endif]-->
+	<script src="../assets/js/main.js"></script>
+
   </body>
 </html>
 ` // Ensure trailing newline
